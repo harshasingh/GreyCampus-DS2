6b077b3b92726dc4de296eaf1370250cea7e88db
Harsha Singh <harshasingh296@gmail.com>
2021-05-01 23:48:46 +0530

    Add files via upload

---
+++ SQL/Projects/SQL Teams.sql
@@ -0,0 +1,71 @@
+1.
+SELECT
+	-- Select the team long name and team API id
+	team_long_name,
+	team_api_id
+FROM teams_germany
+-- Only include FC Schalke 04 and FC Bayern Munich
+WHERE team_long_name IN (FC Schalke 04, FC Bayern Munich);
+
+2.
+-- Identify the home team as Bayern Munich, Schalke 04, or neither
+SELECT 
+	CASE WHEN hometeam_id = 10189 THEN 'FC Schalke 04'
+        WHEN hometeam_id = 9823 THEN 'FC Bayern Munich'
+         ELSE 'Other' END AS home_team,
+	COUNT(id) AS total_matches
+FROM matches_germany
+-- Group by the CASE statement alias
+GROUP BY home_team;
+
+1/3:
+SELECT 
+	-- Select the date of the match
+	date,
+	-- Identify home wins, losses, or ties
+	CASE WHEN home_goal > away_goal THEN 'Home win!'
+        WHEN home_goal < away_goal THEN 'Home loss :(' 
+        ELSE 'Tie' END AS outcome
+FROM matches_spain;
+
+2/3
+SELECT 
+	m.date,
+	--Select the team long name column and call it 'opponent'
+	t.team_long_name AS opponent, 
+	-- Complete the CASE statement with an alias
+	CASE WHEN home_goal > away_goal THEN 'Home win!'
+        WHEN home_goal < away_goal THEN 'Home loss :(' 
+        ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Left join teams_spain onto matches_spain
+	LEFT JOIN teams_spain AS t
+ON m.awayteam_id = t.team_api_id;
+
+3/3
+SELECT 
+	m.date,
+	t.team_long_name AS opponent,
+    -- Complete the CASE statement with an alias
+	CASE WHEN m.home_goal > away_goal THEN  'Barcelona win!'
+        WHEN m.home_goal < away_goal THEN  'Barcelona loss :(' 
+        ELSE 'Tie' END AS outcome 
+FROM matches_spain AS m
+LEFT JOIN teams_spain AS t 
+ON m.awayteam_id = t.team_api_id
+-- Filter for Barcelona as the home team
+WHERE m.hometeam_id = 8634; 
+
+part2
+-- Select matches where Barcelona was the away team
+SELECT  
+	m.date,
+	t.team_long_name AS opponent,
+	CASE WHEN m.home_goal < m.away_goal THEN 'Barcelona win!'
+         WHEN m.home_goal > m.away_goal THEN 'Barcelona loss :(' 
+         ELSE 'Tie' END AS outcome
+FROM matches_spain AS m
+-- Join teams_spain to matches_spain
+LEFT JOIN teams_spain AS t 
+ON m.hometeam_id = t.team_api_id
+WHERE m.hometeam_id = 8634;
